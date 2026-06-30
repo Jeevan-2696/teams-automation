4a4a5080c04d2d1894e30632175efd64983de9bb
hp <hp@DESKTOP-SMUTTVJ>
2026-06-30 18:00:42 +0530

    Added BillingPage

---
+++ src/main/java/com/teams/pages/BillingPage.java
@@ -0,0 +1,47 @@
+package com.teams.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BillingPage {
+
+    WebDriver driver;
+
+    public BillingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By billingMenu = By.xpath("//span[normalize-space()='Billing']/ancestor::button");
+
+    By selectedPlan = By.xpath("//button[.//p[normalize-space()='Premium']]");
+    
+    By continueToPaymentButton = By.xpath("//button[contains(.,'Continue to Payment')]");
+
+    
+    
+    public void clickBillingMenu() {
+        driver.findElement(billingMenu).click();
+    }
+
+    public void clickPremiumSelect() {
+        driver.findElement(selectedPlan).click();
+    }
+
+    public void clickContinueToPayment() {
+        driver.findElement(continueToPaymentButton).click();
+    }
+
+    
+    public void goToPayment() throws InterruptedException {
+        clickBillingMenu();
+
+        Thread.sleep(2000);
+
+        clickPremiumSelect();
+
+        Thread.sleep(2000);
+
+        clickContinueToPayment();
+    }
+}
---
+++ src/test/java/com/teams/tests/BillingTest.java
@@ -0,0 +1,28 @@
+package com.teams.tests;
+
+import org.testng.annotations.Test;
+
+import com.teams.base.BaseClass;
+import com.teams.pages.LoginPage;
+import com.teams.pages.BillingPage;
+
+public class BillingTest extends BaseClass {
+
+    @Test
+    public void verifyPaymentPage() throws InterruptedException {
+
+        
+
+        LoginPage login = new LoginPage(driver);
+
+        login.login("jeevankalyan261@gmail.com", "Admin@1234");
+
+        Thread.sleep(5000);
+
+        BillingPage payment = new BillingPage(driver);
+
+        payment.goToPayment();
+
+        Thread.sleep(5000);
+    }
+}
